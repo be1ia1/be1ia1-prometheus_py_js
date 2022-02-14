5d3fc3ce7bee4dfbb4b1de7f0cf9265ffec70564
Andriy Gula <gula@pr.km.ua>
2022-02-14 22:41:33 +0200

    some home

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('tasks/', include('tasks.urls')),
     path('newyear/', include('newyear.urls')),
     path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls)
